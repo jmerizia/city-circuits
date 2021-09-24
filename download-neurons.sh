a04e303aa9f969789ae50edfdbf50822afafa37e
Jacob Merizian <jmerizia@vt.edu>
2021-09-24 02:13:19 -0400

    use a downloadable index

--- download-neurons.sh
+++
@@ -1,2 +0,0 @@
-mkdir -p data
-aws s3 cp s3://gpt2-neurons/wikipedia-first-lines/neurons.jsonl ./data
---
+++ fetch-index.sh
@@ -0,0 +1,2 @@
+aws s3 cp s3://gpt2-neurons/wikipedia-first-lines/index.zip ./data
+unzip ./data/index.zip -d ./data/index
